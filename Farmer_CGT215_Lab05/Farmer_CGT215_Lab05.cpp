915af00d919820923005d11841d1c705be510f6e
Ethan Farmer <efarmer743@outlook.com>
2023-10-04 23:39:31 -0400

    Testing if statment for working cipher.

--- Farmer_CGT215_Lab05/Farmer_CGT215_Lab05.cpp
+++ Farmer_CGT215_Lab05/Farmer_CGT215_Lab05.cpp
@@ -7,22 +7,36 @@
 
 using namespace std;
 
-// Eventual function for swapping characters within user submited string.
-void cipher() {
+// A vector for character encryption.
+vector<char> cipherCharVector({ 'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' });
 
+// Eventual function for swapping characters within user submited string.
+char cipher(char enteredString) {
+    if ((enteredString >= 65) && (enteredString <= 90)) {
+        return cipherCharVector.at(enteredString - 65);
+    }
+    else if ((enteredString >= 97) && (enteredString <= 122)) {
+        char upperCaseLetter = enteredString - 32;
+        char upperCaseCode = cipherCharVector.at(upperCaseLetter - 65);
+        return upperCaseCode + 32;
+    }
+    else {
+        return enteredString;
+    }
 }
 
 int main() {
     // Sets a variable for getting a user inputed string (including the whitespaces).
     string userString;
-    // Set of vectors for character conversion.
-    vector<char> cipherCharVector({ 'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' });
 
     // Message for user to know program needs input and grabing the users input.
     cout << "Enter a string (up to 128 characters) to encrypt: ";
     getline(cin, userString);
 
     // Outputing the users input but it is now encoded.
-    cout << "Encoded Message: " << userString;
+    cout << "Encoded Message: ";
+    for (int i = 0; i < userString.size(); i++) {
+        cout << cipher(userString.at(i));
+    }
 
 }
